b475b6a2f89a7f5671db2ab495e7411d8b658b2a
Stian Rørlien <stian.rorlien@bouvet.no>
2022-08-05 15:07:21 +0200

    Generert Angular app

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { PowerpriceComponent } from './modules/powerprice/powerprice.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PowerpriceComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/modules/powerprice/powerprice.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-powerprice',
+  templateUrl: './powerprice.component.html',
+  styleUrls: ['./powerprice.component.scss']
+})
+export class PowerpriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
